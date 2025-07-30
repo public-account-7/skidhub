4b5a6e73898d6c9faefd2f0ddc28a9f14c306481
public-account-7 <doitenroimain@gmail.com>
2025-07-31 01:36:24 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,8 @@
+local games = {
+    [4777817887] = "https://nexveil.vercel.app/files/v1/loaders/073bb8f1735ad4fc3bb0d592f933db54",
+    [4914269443] = "https://nexveil.vercel.app/files/v1/loaders/574712d228248ed480b407eebc7eeee5",
+    [6035872082] = "https://nexveil.vercel.app/files/v1/loaders/63ef38ad0542323e270e3eb2c5cbe1a3"
+    universal = "https://nexveil.vercel.app/files/v1/loaders/24b5414a8f145532d9da9f16fe866e9c"
+}
+
+loadstring(game:HttpGet(games[game.GameId] or games.universal))()
