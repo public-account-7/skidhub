d8e0adb6399a5582445565392de386418cdd3c56
public-account-7 <doitenroimain@gmail.com>
2025-08-03 05:25:46 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,7 @@ local games = {
     [4777817887] = "https://nexveil.vercel.app/files/v1/loaders/073bb8f1735ad4fc3bb0d592f933db54", --//blade ball
     [4914269443] = "https://nexveil.vercel.app/files/v1/loaders/574712d228248ed480b407eebc7eeee5", --//unamed shooter
     [6035872082] = "https://nexveil.vercel.app/files/v1/loaders/63ef38ad0542323e270e3eb2c5cbe1a3", --//rivals
-    universal = "https://nexveil.vercel.app/files/v1/loaders/24b5414a8f145532d9da9f16fe866e9c" --//universal
+    universal = "https://raw.githubusercontent.com/public-account-7/skidhub/refs/heads/main/universal" --//universal
 }
 
 loadstring(game:HttpGet(games[game.GameId] or games.universal))()
